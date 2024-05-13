65fd3d02eda9dd64623e49621df3e22abc293d3e
Kene Nduka <ndukasquad@gmail.com>
2024-05-13 08:51:32 +0100

    Initial Commit

---
+++ src/components/HomeCards.jsx
@@ -0,0 +1,25 @@
+import Card from "./Card"
+import { Link } from "react-router-dom"
+
+const HomeCards = () => {
+  return (
+    <section className="py-4">
+        <div className="container-xl lg:container m-auto">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+                <Card>
+                    <h2 className="text-2xl font-bold">For Developers</h2>
+                    <p className="mt-2 mb-4">View available jobs and begin your career today</p>
+                    <Link to='/jobs' className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700">Browse</Link>
+                </Card>
+                <Card bg="bg-indigo-700">
+                    <h2 className="text-2xl font-bold">For Employers</h2>
+                    <p className="mt-2 mb-4">List your job and find the perfect employee for the role.</p>
+                    <Link to='/add-job' className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600">Add Job</Link>
+                </Card>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default HomeCards
